519cdb2b533c675c5911096a15eae5144f5e2c62
Timm Friebe <thekid@thekid.de>
2013-11-12 15:12:02 +0100

    Initial release: IRC client Extracted from XP Framework core as described in xp-framework/rfc#262

--- src/main/php/peer/irc/IRCColor.class.php
+++ src/main/php/peer/irc/IRCColor.class.php
@@ -1,52 +1,47 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace peer\irc;
+
+define('IRC_COLOR_DEFAULT',     0);
+define('IRC_COLOR_BLACK',       1);
+define('IRC_COLOR_DARKBLUE',    2);
+define('IRC_COLOR_DARKGREEN',   3);
+define('IRC_COLOR_RED',         4);
+define('IRC_COLOR_PURPLE',      5);
+define('IRC_COLOR_DARKRED',     6);
+define('IRC_COLOR_ORANGE',      7);
+define('IRC_COLOR_YELLOW',      8);
+define('IRC_COLOR_GREEN',       9);
+define('IRC_COLOR_MAGENTA',    10);
+define('IRC_COLOR_STEELBLUE',  11);
+define('IRC_COLOR_BLUE',       12);
+define('IRC_COLOR_PINK',       13);
+define('IRC_COLOR_DARKGRAY',   14);
+define('IRC_COLOR_LIGHTGRAY',  15);
+define('IRC_COLOR_WHITE',      16);
+
+
+/**
+ * IRC colors
  *
- * $Id$ 
+ * Example:
+ * <code>
+ *   $connection->sendMessage(
+ *     '#test', 
+ *     '%sThis is red', 
+ *     IRCColor::forCode(IRC_COLOR_RED)
+ *   );
+ * </code>
+ *
+ * @purpose  Utility class
  */
-
-  define('IRC_COLOR_DEFAULT',     0);
-  define('IRC_COLOR_BLACK',       1);
-  define('IRC_COLOR_DARKBLUE',    2);
-  define('IRC_COLOR_DARKGREEN',   3);
-  define('IRC_COLOR_RED',         4);
-  define('IRC_COLOR_PURPLE',      5);
-  define('IRC_COLOR_DARKRED',     6);
-  define('IRC_COLOR_ORANGE',      7);
-  define('IRC_COLOR_YELLOW',      8);
-  define('IRC_COLOR_GREEN',       9);
-  define('IRC_COLOR_MAGENTA',    10);
-  define('IRC_COLOR_STEELBLUE',  11);
-  define('IRC_COLOR_BLUE',       12);
-  define('IRC_COLOR_PINK',       13);
-  define('IRC_COLOR_DARKGRAY',   14);
-  define('IRC_COLOR_LIGHTGRAY',  15);
-  define('IRC_COLOR_WHITE',      16);
-
+class IRCColor extends \lang\Object {
 
   /**
-   * IRC colors
+   * Retrieves color representation
    *
-   * Example:
-   * <code>
-   *   $connection->sendMessage(
-   *     '#test', 
-   *     '%sThis is red', 
-   *     IRCColor::forCode(IRC_COLOR_RED)
-   *   );
-   * </code>
-   *
-   * @purpose  Utility class
+   * @param   int code
+   * @return  string color representation
    */
-  class IRCColor extends Object {
-  
-    /**
-     * Retrieves color representation
-     *
-     * @param   int code
-     * @return  string color representation
-     */
-    public static function forCode($code) {
-      return "\3".$code;
-    }
+  public static function forCode($code) {
+    return "\3".$code;
   }
-?>
+}
